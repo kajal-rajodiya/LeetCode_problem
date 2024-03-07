14f9f17f624d81838ae0882786a5bf1695c0a6a9
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-03-07 10:39:15 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,1 +1,28 @@
-
+public class Soluation{
+  int low=0;
+  int high=num;
+  int mid;
+  while(low<=high)
+  {
+    if(Array.mid[]==target)
+    {
+      return mid;
+    }
+    else if(Array.mid[]<=target)
+    {
+      low=mid+1;
+    }
+    else
+    {
+      high=mid-1;
+    }
+  }
+return -1;
+  public static void main(String args[])
+  {
+    int Array[]={2,3,4,7,97,77,09};
+    int num=Array.length-1;
+    int target=3;
+    System.out.println(Soluation(A,target));
+  }
+}
